6d3b7fbad9b2428396c6ad8c14eab9bce1f78961
jarcik <j.otmanova@gmail.com>
2020-03-27 14:09:32 +0100

    Upravy

--- app/src/main/java/com/example/android/footballcounter/MainActivity.java
+++ app/src/main/java/com/example/android/footballcounter/MainActivity.java
@@ -18,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
      */
     private int pointsB = 0;
 
+    /**
+     * Fauls for A team
+     */
+    private int faulsA = 0;
+    /**
+     * Fauls for B team
+     */
+    private int faulsB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,18 +67,30 @@ public class MainActivity extends AppCompatActivity {
         String tag = (String) view.getTag();
         //add points
         addPoints(points, tag);
+        //add fauls
+        addFaul(tag);
     }
 
     /**
-     * Reseting all points
+     * Reseting all points and fauls
      */
     public void reset(View view) {
+        //reset points and fauls
         pointsA = 0;
         pointsB = 0;
+        faulsA = 0;
+        faulsB = 0;
+
+        //set points
         TextView aTextView = findViewById(R.id.pointsA);
         aTextView.setText(String.valueOf(pointsA));
         TextView bTextView = findViewById(R.id.pointsB);
         bTextView.setText(String.valueOf(pointsB));
+        //set fauls
+        TextView afTextView = findViewById(R.id.faulA);
+        afTextView.setText(String.valueOf(faulsA));
+        TextView bfTextView = findViewById(R.id.faulB);
+        bfTextView.setText(String.valueOf(faulsB));
     }
 
     /**
@@ -104,5 +125,38 @@ public class MainActivity extends AppCompatActivity {
         if(textView == null) return;
         //write points to the view
         textView.setText(String.valueOf(countedPoints));
+    }
+
+    /**
+     * add specified count of fauls to specified Team
+     * @param team to which team add faul
+     */
+    private void addFaul(String team) {
+        //init of textview for fauls to specific team
+        TextView textView = null;
+        //init of number of fauls to specific team
+        int countedFauls = 0;
+        //decide the team and global varialbles
+        switch (team) {
+            case "A":
+                //assign textview for points
+                textView = findViewById(R.id.faulA);
+                //count fauls to global variables
+                faulsA++;
+                //assing fauls for the view
+                countedFauls = faulsA;
+                break;
+            case "B":
+                //assign textview for points
+                textView = findViewById(R.id.faulB);
+                //count fauls to global variables
+                faulsB++;
+                //assing fauls for the view
+                countedFauls = faulsB;
+                break;
+        }
+        if(textView == null) return;
+        //write points to the view
+        textView.setText(String.valueOf(countedFauls));
     }
 }
